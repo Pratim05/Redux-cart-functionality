bd09c42f2768b870d7523eac7cde9c77f13914ef
Pratim Bera <101959071+Pratim05@users.noreply.github.com>
2024-06-20 14:06:14 +0000

    Redux functionality

--- redux-cart-function/src/Components/Cart.jsx
+++ redux-cart-function/src/Components/Cart.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
+import { FaCartArrowDown } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+
+import '../Css/Cart.css'
+
+import { useSelector } from 'react-redux';
+
 
 function Cart() {
+  const items = useSelector((state)=>state)
+  console.log('Items', items)
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div>Cart</div>
+    <div className='CartArea'>
+      <button onClick={toggleDropdown}>{isOpen ? <ImCross /> : <FaCartArrowDown />}</button>
+      {isOpen && (
+        <div className='CartItems'>
+          <h1>Total No of Items : {items.cart.length}</h1>
+          {items.cart.map((item)=>{
+            return <div className='item'>
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+              <button><ImCross /></button>
+            </div>
+            
+          })}
+
+        </div>
+      )}
+
+    </div>
   )
 }
 
--- redux-cart-function/src/Components/Product.jsx
+++ redux-cart-function/src/Components/Product.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import "../Css/Product.css"
+import { addItem } from '../redux/slices/cartSlice'
 
 function Product(props) {
-    console.log(props.thumbnail)
+  const dishpatch = useDispatch()
+  
   return (
     <div className='Product'>
         <div className='Product-Box'>
             <div className='img-box'><img src={props.thumbnail} alt=""  /></div>
             <h2>{props.title}</h2>
             <p>{props.description}</p>
+            <p>Price : {props.price}</p>
         </div>
+        <button className='CartBtn' onClick={(e)=>dishpatch(addItem({
+          name:props.title, price:props.price
+        }))}>Add to Cart</button>
     </div>
   )
 }
